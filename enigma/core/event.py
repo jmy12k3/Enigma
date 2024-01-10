dd8dc5d8860e876842eeb007c37ca0c2c0a77f8b
Kaho Fan <jmy.12k3@gmail.com>
2024-01-10 17:07:22 +0800

    Update type hint in EventEngine class

--- enigma/core/event.py
+++ enigma/core/event.py
@@ -8,7 +8,7 @@ from enigma.core.enum import EventType
 from enigma.core.logging import logger
 from enigma.core.object import Event
 
-HandlerType = Callable[[Event], None]
+_T = Callable[[Event], None]
 
 
 class EventEngine:
@@ -18,7 +18,7 @@ class EventEngine:
         self._active = False
         self._thread = Thread(target=self._run)
         self._timer = Thread(target=self._run_timer)
-        self._handlers: defaultdict[EventType, list[HandlerType]] = defaultdict(list)
+        self._handlers: defaultdict[EventType, list[_T]] = defaultdict(list)
 
     def _run(self) -> None:
         while self._active:
@@ -50,7 +50,7 @@ class EventEngine:
     def put(self, event: Event) -> None:
         self._queue.put(event)
 
-    def register(self, type: EventType, handler: HandlerType) -> None:
+    def register(self, type: EventType, handler: _T) -> None:
         handler_list = self._handlers[type]
         if handler not in handler_list:
             handler_list.append(handler)
@@ -60,7 +60,7 @@ class EventEngine:
                 Handler=handler.__name__,
             )
 
-    def unregister(self, type: EventType, handler: HandlerType) -> None:
+    def unregister(self, type: EventType, handler: _T) -> None:
         handler_list = self._handlers[type]
         if handler in handler_list:
             handler_list.remove(handler)
