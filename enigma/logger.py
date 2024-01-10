0fbdd95ea5b6339d1ae93f75a6b75059fe6c16ff
Kaho Fan <jmy.12k3@gmail.com>
2024-01-11 00:41:08 +0800

    Add enigma logger module

---
+++ enigma/logger.py
@@ -0,0 +1,53 @@
+"""A loguru-like logger that uses structlog."""
+
+import logging
+import sys
+from functools import cache
+from typing import Any, TextIO
+
+import structlog
+
+
+@cache
+def _logger(*, stream: TextIO) -> Any:
+    shared_processors: list[structlog.typing.Processor] = [
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+    ]
+    processors: list[structlog.typing.Processor] = [*shared_processors]
+
+    if stream.isatty():
+        # Development - pretty printing
+        level = logging.DEBUG
+        processors.extend(
+            [
+                structlog.processors.TimeStamper("%Y-%m-%d %H:%M:%S", utc=False),
+                structlog.dev.ConsoleRenderer(),
+            ],
+        )
+    else:
+        # Production - structured logging
+        level = logging.INFO
+        processors.extend(
+            [
+                structlog.processors.dict_tracebacks,
+                structlog.processors.TimeStamper("iso"),
+                structlog.processors.JSONRenderer(),
+            ],
+        )
+
+    structlog.configure(
+        processors,
+        structlog.stdlib.BoundLogger,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(format="%(message)s", level=level, stream=stream)
+
+    return structlog.get_logger()
+
+
+logger = _logger(stream=sys.stdout)
