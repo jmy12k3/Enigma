d643f99fae5a7b9b9fcaaa7a090e8e7e7c94eae1
Kaho Fan <jmy.12k3@gmail.com>
2024-01-11 01:19:27 +0800

    Update database configuration in config.py

--- enigma/config.py
+++ enigma/config.py
@@ -3,17 +3,20 @@
 import tzlocal
 from dynaconf import Dynaconf
 
+# This should be loaded from a .env file
 CONFIG = {
-    "DATABASE_TIMEZONE": tzlocal.get_localzone_name(),
     "DATABASE_NAME": "",
     "DATABASE_DATABASE": "",
     "DATABASE_HOST": "",
-    "DATABASE_PORT": 0,
+    "DATABASE_PORT": "",
     "DATABASE_USER": "",
     "DATABASE_PASSWORD": "",
     "DATAFEED_NAME": "",
     "DATAFEED_USERNAME": "",
     "DATAFEED_PASSWORD": "",
 }
+
+# This should be loaded from a environemnetal variable
+CONFIG["DATABASE_TIMEZONE"] = tzlocal.get_localzone_name()
 
 CONFIG.update(Dynaconf(envvar_prefix=False, load_dotenv=True).to_dict())
