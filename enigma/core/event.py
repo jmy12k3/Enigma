d74cdce72eab3b9027a58227403f94df3b433290
Kaho Fan <jmy.12k3@gmail.com>
2024-01-11 17:42:06 +0800

    Refactor event handling in EventEngine class

--- enigma/core/event.py
+++ enigma/core/event.py
@@ -1,12 +1,11 @@
 import time
 from collections import defaultdict
 from collections.abc import Callable
+from contextlib import suppress
 from queue import Empty, Queue
 from threading import Thread
 
-from enigma.core.enum import EventType
-from enigma.core.logging import logger
-from enigma.core.object import Event
+from enigma.models import Event, EventType
 
 _T = Callable[[Event], None]
 
@@ -22,15 +21,13 @@ class EventEngine:
 
     def _run(self) -> None:
         while self._active:
-            try:
-                event: Event = self._queue.get(block=True, timeout=1)
-                self._process(event)
-            except Empty:
-                pass
+            with suppress(Empty):
+                self._process(self._queue.get(block=True, timeout=1))
 
     def _process(self, event: Event) -> None:
         if event.type in self._handlers:
-            [handler(event) for handler in self._handlers[event.type]]
+            for handler in self._handlers[event.type]:
+                handler(event)
 
     def _run_timer(self) -> None:
         while self._active:
@@ -54,20 +51,10 @@ class EventEngine:
         handler_list = self._handlers[type]
         if handler not in handler_list:
             handler_list.append(handler)
-            logger.debug(
-                "Registered event handler",
-                Event=str(type),
-                Handler=handler.__name__,
-            )
 
     def unregister(self, type: EventType, handler: _T) -> None:
         handler_list = self._handlers[type]
         if handler in handler_list:
             handler_list.remove(handler)
-            logger.debug(
-                "Unregistered event handler",
-                event=str(type),
-                handler=handler.__name__,
-            )
         if not handler_list:
             del self._handlers[type]
