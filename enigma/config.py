70abeb453cbf636285e13920da7bd427db094d13
Kaho Fan <jmy.12k3@gmail.com>
2024-01-12 21:40:07 +0800

    TEST

--- enigma/config.py
+++ enigma/config.py
@@ -1,35 +1,12 @@
-"""Config in twelve-factor standard."""
+import os
+import re
 
-import dynaconf
-import tzlocal
+import dotenv
 
-# set envvar_prefix as package name in upper case
-ENVVAR_PREFIX = __package__.upper()
-
-# set default values for optional settings
-# note that dotenv config does not support nested structures
-CONFIG = {
-    "DATABASE_NAME": "",
-    "DATABASE_DATABASE": "",
-    "DATABASE_HOST": "",
-    "DATABASE_PORT": "",
-    "DATABASE_USER": "",
-    "DATABASE_PASSWORD": "",
-    "DATAFEED_NAME": "",
-    "DATAFEED_USERNAME": "",
-    "DATAFEED_PASSWORD": "",
+_PREFIX = __package__.upper()
+_REGEX = re.compile(rf"{_PREFIX}\w+")
+_ENVIRONS = {
+    k.replace(_PREFIX, ""): v for k, v in os.environ.items() if _REGEX.match(k)
 }
 
-# set default values for mandatory setting
-CONFIG["DATABASE_TIMEZONE"] = tzlocal.get_localzone_name()
-
-# append envvar_prefix to default config
-CONFIG = {f"{ENVVAR_PREFIX}{k}": v for k, v in CONFIG.items()}
-
-# load config from environment variables and dotenv file
-CONFIG.update(
-    dynaconf.Dynaconf(
-        envvar_prefix=ENVVAR_PREFIX,
-        load_dotenv=True,
-    ).to_dict()
-)
+CONFIG = {**dotenv.dotenv_values(".env"), **_ENVIRONS}
