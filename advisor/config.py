4f5afb464815c486783b8d3d64651a5a62fbb80b
Kaho Fan <jmy.12k3@gmail.com>
2024-01-13 06:17:13 +0800

    Add timezone configuration to advisor/config.py

--- advisor/config.py
+++ advisor/config.py
@@ -4,6 +4,7 @@ from functools import cache
 from typing import Any
 
 import dotenv
+import tzlocal
 
 
 @cache
@@ -13,6 +14,7 @@ def _get_config(*, prefixes: str = __package__.upper()) -> dict[str, Any]:
     return {
         k.replace(prefixes, "").upper(): v
         for k, v in {
+            "DATABASE_TIMEZONE": tzlocal.get_localzone_name(),
             **dotenv.dotenv_values(".env"),
             **{k: v for k, v in os.environ.items() if regex.match(k)},
         }.items()
