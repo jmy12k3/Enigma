b5d57a88c7420f5776efc05b4e74c244dd827052
Kaho Fan <jmy.12k3@gmail.com>
2024-01-12 21:44:49 +0800

    Update database and datafeed configuration

--- enigma/config.py
+++ enigma/config.py
@@ -5,8 +5,8 @@ import dotenv
 
 _PREFIX = __package__.upper()
 _REGEX = re.compile(rf"{_PREFIX}\w+")
-_ENVIRONS = {
-    k.replace(_PREFIX, ""): v for k, v in os.environ.items() if _REGEX.match(k)
-}
 
-CONFIG = {**dotenv.dotenv_values(".env"), **_ENVIRONS}
+CONFIG = {
+    **dotenv.dotenv_values(".env"),
+    **{k.replace(_PREFIX, ""): v for k, v in os.environ.items() if _REGEX.match(k)},
+}
