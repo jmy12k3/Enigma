dad876ff6c169b9a744e90dbe64ecb0df5a81020
Kaho Fan <jmy.12k3@gmail.com>
2024-01-13 05:39:23 +0800

    Refactor config.py to use functools.cache

--- advisor/config.py
+++ advisor/config.py
@@ -1,15 +1,23 @@
+import functools
 import os
 import re
+from typing import Any
 
 import dotenv
 
-_PREFIXES = f"{__package__.upper()}_"
-_REGEX = re.compile(rf"{_PREFIXES}\w+")
 
-CONFIG = {
-    k.replace(_PREFIXES, "").upper(): v
-    for k, v in {
-        **dotenv.dotenv_values(".env"),
-        **{k: v for k, v in os.environ.items() if _REGEX.match(k)},
-    }.items()
-}
+@functools.cache
+def _get_config() -> dict[str, Any]:
+    prefixes = f"{__package__.upper()}_"
+    regex = re.compile(rf"{prefixes}\w+")
+
+    return {
+        k.replace(prefixes, "").upper(): v
+        for k, v in {
+            **dotenv.dotenv_values(".env"),
+            **{k: v for k, v in os.environ.items() if regex.match(k)},
+        }.items()
+    }
+
+
+CONFIG = _get_config()
