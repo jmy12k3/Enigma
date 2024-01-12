192cf56a934872dddab5a4e9c15e173bed592ccf
Kaho Fan <jmy.12k3@gmail.com>
2024-01-12 23:42:49 +0800

    Refactor configuration loading in enigma/config.py

--- enigma/config.py
+++ enigma/config.py
@@ -4,10 +4,12 @@ import re
 import dotenv
 
 _PREFIXES = f"{__package__.upper()}_"
-
 _REGEX = re.compile(rf"{_PREFIXES}\w+")
 
 CONFIG = {
-    **dotenv.dotenv_values(".env"),
-    **{k.replace(_PREFIXES, ""): v for k, v in os.environ.items() if _REGEX.match(k)},
+    k.replace(_PREFIXES, "").upper(): v
+    for k, v in {
+        **dotenv.dotenv_values(".env"),
+        **{k: v for k, v in os.environ.items() if _REGEX.match(k)},
+    }.items()
 }
