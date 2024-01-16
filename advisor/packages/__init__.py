10d270f0f3242c21cef9bfadca4c34e66880e395
Kaho Fan <jmy.12k3@gmail.com>
2024-01-16 15:49:10 +0800

    Add PackageImportError class for package import exception handling

--- advisor/packages/__init__.py
+++ advisor/packages/__init__.py
@@ -2,5 +2,9 @@ from importlib import import_module
 from types import ModuleType
 
 
+class PackageImportError(Exception):
+    """Provide an exception for the package import."""
+
+
 def import_package(package_name: str) -> ModuleType:
     return import_module(f".{package_name}", __package__)
